3823f313bd92d7c2ca77d845b0328a1f76f30fda
Shubham Salunke <shubhamdeepak007@gmail.com>
2026-03-31 17:23:38 +0530

    Added single axiosInstance and configured req/res interceptors

---
+++ App/src/services/axiosInstance.ts
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import axios from "axios";
+
+config();
+
+const baseUrl = process.env.VITE_API_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = ""; // TODO: Get token from store
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (error.response?.status === 401) {
+      // TODO: Clear state
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
